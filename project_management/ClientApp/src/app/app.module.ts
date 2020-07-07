d2ebc3bd82f1002eba078c28a0f757a48897ab41
wasitthaphon <wasitthaphon@gmail.com>
2020-07-07 20:03:10 +0700

    add new nav item

--- project_management/ClientApp/src/app/app.module.ts
+++ project_management/ClientApp/src/app/app.module.ts
@@ -3,11 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { ModalModule } from 'ngx-bootstrap/modal';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgbDropdown, NgbDropdownMenu, NgbDropdownItem, NgbDropdownToggle } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -40,12 +38,17 @@ import { CreateProjectProgressComponent } from './create-project-progress/create
     CreateProjectProgressComponent,
   ],
   imports: [
+    BrowserModule,
+    NgbDropdown,
+    NgbDropdownItem,
+    NgbDropdownMenu,
+    NgbDropdownToggle,
     BrowserAnimationsModule,
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      {path: '', component: LoginComponent},
+      {path: '/', component: LoginComponent},
       {path: 'register', component: RegisterComponent},
       {path: 'home', component: HomeComponent},
       {path: 'tasks', component: MainTasksComponent},
@@ -55,10 +58,6 @@ import { CreateProjectProgressComponent } from './create-project-progress/create
       {path: 'create-project-members', component: CreateProjectMembersComponent},
       {path: 'create-project-tasks', component: CreateProjectTasksComponent},
     ]),
-    BrowserModule,
-    BsDropdownModule.forRoot(),
-    TooltipModule.forRoot(),
-    ModalModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
